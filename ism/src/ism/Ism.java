32873d06c3eaff43f00c5256dc555b09ecb3849a
John Oliver G Virola <106477188+John-Oliver04@users.noreply.github.com>
2023-09-13 14:10:51 +0800

    Add files via upload

---
+++ ism/src/ism/Ism.java
@@ -0,0 +1,16 @@
+
+package ism;
+
+import javax.swing.JFrame;
+import ui.Menu;
+
+public class Ism extends JFrame{
+
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        
+    }
+    
+}
